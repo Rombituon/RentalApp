9acaf2b3ee56fad67d87669d3670c6706f35c9d6
Jurin Liyun <jurin@ums.edu.my>
2023-06-12 20:45:30 +0800

    add fix for custom route for api

---
+++ Api/AnotherRenterController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RentalApp.Data;
+using RentalApp.Models;
+
+namespace RentalApp.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnotherRenterController : ControllerBase
+    {
+        private readonly RentalDataContext _context;
+
+        public AnotherRenterController(RentalDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/AnotherRenter/GetAll
+
+
+        [HttpGet()]
+        [Route("GetAll")]
+        public async Task<ActionResult<IEnumerable<Renter>>> GetRenters()
+        {
+            return await _context.Renters.ToListAsync();
+        }
+    }
+}
